706a13a3c70667a44abc2d7a5823bc8e4968a232
thiago.vmatos <thiago.vmatos@noleak.io>
2024-08-19 22:14:04 -0300

    Modified thresholds

--- app/main/processor/enhancement_processor.py
+++ app/main/processor/enhancement_processor.py
@@ -3,7 +3,8 @@ import os
 import cv2 # type: ignore
 import numpy as np # type: ignore
 from PIL import Image, ImageOps # type: ignore
-from skimage import filters, img_as_ubyte # type: ignore
+from skimage import img_as_ubyte
+from skimage.filters import threshold_sauvola, threshold_otsu
 import concurrent.futures
 from ..processor.abstract_processor import AbstractProcessor
 from ..service.msthgr_service import MSTHGRService
@@ -39,12 +40,12 @@ class EnhancementProcessor(AbstractProcessor):
         :return:
         """
         use_threads = True
-        print("DEBUG: Starting processing with methods:", self.methods)
+        print("DEBUG: ", self.FILE_NAME, 'process', f"Starting processing with methods: {self.methods}")
         
         cropped_images_directory = input_data.get("cropped_images_directory", "")
         
         if not cropped_images_directory:
-            print("ERROR: No cropped_images_directory specified in input_data.")
+            print("ERROR: ", self.FILE_NAME, 'process', 'No cropped_images_directory specified in input_data.')
             return input_data
 
         # Listar todas as imagens no diretório
@@ -60,26 +61,22 @@ class EnhancementProcessor(AbstractProcessor):
                 if method_func:
                     input_data, image = method_func(input_data, image)  # Passa a imagem para o método e recebe a imagem processada de volta
                 else:
-                    print(f"ERROR: Method {method} is not implemented.")
+                    print("ERROR: ", self.FILE_NAME, 'process', f'Method {method} is not implemented."')
                     continue
             
-            # Sobrescrever a imagem processada no mesmo diretório
+            # Sobrescrita da imagem
             image.save(image_path)
             return image_file, input_data
         
         if use_threads:
-            # Processamento paralelo com threads
             with concurrent.futures.ThreadPoolExecutor() as executor:
                 futures = [executor.submit(process_image, image_file, input_data) for image_file in image_files]
                 for future in concurrent.futures.as_completed(futures):
                     image_file, input_data = future.result()
-
         else:
-            # Processamento normal em série
             for image_file in image_files:
                 _, input_data = process_image(image_file, input_data)
         
-        print("INFO: Finished enhancement_processor.py")
         return input_data
 
     def get_config(self) -> list:
@@ -110,46 +107,42 @@ class EnhancementProcessor(AbstractProcessor):
             
         return input_data, msthgr_img
 
-    def gray_scale(self, input_data: dict, image: Image.Image) -> tuple:
+    def gray_scale(self, input_data: dict, image) -> tuple:
         """
         Convert the image to grayscale.
         """
-        return image.convert("L"), input_data
-    
+        if isinstance(image, Image.Image):
+            image_np = np.array(image.convert("L"))
+        else:
+            if len(image.shape) > 2:
+                image_np = np.mean(image, axis=2).astype(np.uint8)
+            else:
+                image_np = image
+        
+        return input_data, image_np
+
     def sauvola_threshold(self, input_data: dict, image: Image.Image) -> tuple:
         """
         Applies the Sauvola threshold in the image.
         """
-        image_np = np.array(image)
+        input_data, image_np = self.gray_scale(input_data, image)
         
-        # Convertendo para escala de cinza se não for
-        if len(image_np.shape) > 2:
-            image_np = self.gray_scale(image_np, input_data)
-        
-        # Aplicando Sauvola threshold
-        thresh_sauvola = filters.threshold_sauvola(image_np)
+        thresh_sauvola = threshold_sauvola(image_np, window_size=25, k=0.15)
         binary_sauvola = image_np > thresh_sauvola
         
-        # Convertendo de volta para imagem PIL
         binary_sauvola_img = Image.fromarray(img_as_ubyte(binary_sauvola))
-        
+
         return input_data, binary_sauvola_img
-    
+
     def otsu_threshold(self, input_data: dict, image: Image.Image) -> tuple:
         """
         Applies the Otsu threshold in the image.
         """
-        image_np = np.array(image)
+        input_data, image_np = self.gray_scale(input_data, image)
         
-        # Convertendo para escala de cinza se não for
-        if len(image_np.shape) > 2:
-            image_np = self.gray_scale(image_np, input_data)
-        
-        # Aplicando Otsu threshold
-        thresh_otsu = filters.threshold_otsu(image_np)
+        thresh_otsu = threshold_otsu(image_np, nbins=256)
         binary_otsu = image_np > thresh_otsu
         
-        # Convertendo de volta para imagem PIL
         binary_otsu_img = Image.fromarray(img_as_ubyte(binary_otsu))
         
         return input_data, binary_otsu_img
@@ -162,8 +155,12 @@ class EnhancementProcessor(AbstractProcessor):
         :param threshold: Valor de threshold para a binarização (0-255).
         :return: Imagem PIL binarizada.
         """
-        binary_image = image.point(lambda p: 255 if p > threshold else 0, mode='1')
+        input_data, image_np = self.gray_scale(input_data, image)
     
+        binary_np = np.where(image_np > threshold, 255, 0).astype(np.uint8)
+        
+        binary_image = Image.fromarray(binary_np)
+        
         return input_data, binary_image
 
     def pad_image_to_max_dimensions(self, input_data: dict, image: Image.Image) -> tuple:
@@ -196,12 +193,11 @@ class EnhancementProcessor(AbstractProcessor):
         adicionando bordas pretas se necessário para manter a proporção.
         
         :param image: Imagem PIL a ser processada.
-        :param metrics: Dicionário com as métricas 'min_width', 'max_width', 
-                        'min_height', 'max_height'.
+        :param metrics: Dicionário com as métricas 'avg_width', 'avg_width'
         :return: Imagem PIL com as dimensões padronizadas.
         """
-        avg_width = (input_data['metrics']['min_width'] + input_data['metrics']['max_width']) // 2
-        avg_height = (input_data['metrics']['min_height'] + input_data['metrics']['max_height']) // 2
+        avg_width = input_data['metrics']['avg_width']
+        avg_height = input_data['metrics']['avg_height']
         
         # Redimensiona a imagem para caber dentro das dimensões médias mantendo a proporção
         image.thumbnail((avg_width, avg_height), Image.LANCZOS)
@@ -211,5 +207,6 @@ class EnhancementProcessor(AbstractProcessor):
         delta_height = avg_height - image.height
         padding = (delta_width // 2, delta_height // 2, delta_width - (delta_width // 2), delta_height - (delta_height // 2))
         
-        # Adiciona bordas pretas ao redor da imagem para atingir as dimensões médias
+        # Adiciona bordas pretas ao redor da imagem para atingir as dimensões médias da imagem
         return input_data, ImageOps.expand(image, padding, fill='black')
+    
